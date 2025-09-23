5e84b2dcd0a17391384b588513076aeb3f2a5045
Oladipupo Taiwo <samuelt4christ@gmail.com>
2025-09-23 06:22:06 +0100

    checked and fixed

--- lib/src/feature/Presentation/view/widgets/new_editor_screen.dart
+++ lib/src/feature/Presentation/view/widgets/new_editor_screen.dart
@@ -986,7 +986,6 @@ class NewEditorScreenState extends ConsumerState<NewEditorScreen> {
     //scrollPosition.
     setState(() {
       // [totalProgressMap] contains the scrollPosition and the video Data.
-      print(totalProgressMap);
       totalInteractionProgress =
           (totalProgressMap.values.fold(
             0.0,
